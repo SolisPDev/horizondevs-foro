cd8fb42f49d930886b9ac3e650796f04e75ce513
SolisPDev <solispantonio@gmail.com>
2025-01-07 17:15:18 -0600

    Configuracion inicial del proyecto

---
+++ src/main/java/net/horizondevs/horizondevs_foro/HorizondevsForoApplication.java
@@ -0,0 +1,13 @@
+package net.horizondevs.horizondevs_foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizondevsForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizondevsForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/horizondevs/horizondevs_foro/HorizondevsForoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.horizondevs.horizondevs_foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizondevsForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
